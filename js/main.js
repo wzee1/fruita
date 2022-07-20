93fe486a72e674f53e2cf3004ac71a4b8f6b4885
wzee1 <108935996+wzee1@users.noreply.github.com>
2022-07-20 14:38:05 +0200

    Add Files

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const hamburgerIcon = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+const navLinks = document.querySelectorAll(".nav__links");
+const logo = document.getElementById("logo");
+
+// Toggles the menu
+hamburgerIcon.addEventListener("click", () => {
+   menu.classList.toggle("nav-active");
+})
+
+// Removes menu if a link is clicked
+navLinks.forEach(navLinks => {
+   navLinks.addEventListener("click", () => {
+      menu.classList.remove("nav-active");
+   });
+});
+
+// Removes menu if logo is clicked
+logo.addEventListener("click", () => {
+   menu.classList.remove("nav-active");
+})
